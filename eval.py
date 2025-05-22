b6be04a5228032368795ef71623ec2c30ea23f2f
MLDK <davidkohav@gmail.com>
2025-05-22 03:46:41 +0300

    Create eval.py

---
+++ eval.py
@@ -0,0 +1,10 @@
+import pandas
+import torch
+import numpy
+print(f"Pandas version: {pandas.__version__}")
+print(f"PyTorch version: {torch.__version__}")
+print(f"NumPy version: {numpy.__version__}")
+# Test PyTorch
+x = torch.rand(5, 3)
+print(x)
+exit()
